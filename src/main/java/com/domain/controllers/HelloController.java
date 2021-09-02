498ee0a1643ea84d12f9991a0da44a88974abb87
Rendhy 'k <rendika.debug@gmail.com>
2021-09-02 09:22:39 +0700

    change com domain

--- src/main/java/com/domain/controllers/HelloController.java
+++ src/main/java/com/domain/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package kydner.springbase.controllers;
+package com.domain.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
